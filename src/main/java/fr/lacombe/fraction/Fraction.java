97690ac27b62b67d25ba23ae43f08a2edd9591b1
Mohamed Yassine YAHYAOUI <yassine.yahyaoui@lacombedulionvert.fr>
2022-11-22 15:52:25 +0100

    feat: addition of 0 and 5

--- src/main/java/fr/lacombe/fraction/Fraction.java
+++ src/main/java/fr/lacombe/fraction/Fraction.java
@@ -2,15 +2,24 @@ package fr.lacombe.fraction;
 
 public class Fraction {
 
-    public Fraction(int i) {
 
+    private final int value;
+
+    public Fraction(int i) {
+        value = i;
     }
 
     public Fraction addition(Fraction fraction) {
+        if(value == 5){
+            return new Fraction(5);
+        }
         return new Fraction(0);
     }
 
     public int getValue() {
+        if(value == 5){
+            return 5;
+        }
         return 0;
     }
 }
--- src/test/java/fr/lacombe/fraction/AdditionTest.java
+++ src/test/java/fr/lacombe/fraction/AdditionTest.java
@@ -14,4 +14,12 @@ public class AdditionTest {
         Assertions.assertEquals(0, sommeFraction.getValue());
     }
 
+    @Test
+    void given_five_plus_zero_should_return_five(){
+        Fraction fraction1 = new Fraction(5);
+        Fraction fraction2 = new Fraction(0);
+        Fraction sommeFraction = fraction1.addition(fraction2);
+
+        Assertions.assertEquals(5, sommeFraction.getValue());
+    }
 }
