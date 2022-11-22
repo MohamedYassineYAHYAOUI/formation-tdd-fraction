9a3b05455730ce9b9d406a375660279047b5e00e
Mohamed Yassine YAHYAOUI <yassine.yahyaoui@lacombedulionvert.fr>
2022-11-22 15:30:33 +0100

    refacto : change package name

--- src/main/java/fr/lacombe/fraction/Fraction.java
+++ src/main/java/fr/lacombe/fraction/Fraction.java
@@ -1,4 +1,4 @@
-package org.example;
+package fr.lacombe.fraction;
 
 public class Fraction {
 
--- src/test/java/fr/lacombe/fraction/AdditionTest.java
+++ src/test/java/fr/lacombe/fraction/AdditionTest.java
@@ -1,4 +1,4 @@
-package org.example;
+package fr.lacombe.fraction;
 ;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
