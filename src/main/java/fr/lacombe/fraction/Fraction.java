0f2d785d0c7e695fd4137ccc97ca8fb62db87ad1
Mohamed Yassine YAHYAOUI <yassine.yahyaoui@lacombedulionvert.fr>
2022-11-22 17:45:05 +0100

    refacto: collapse condition

--- src/main/java/fr/lacombe/fraction/Fraction.java
+++ src/main/java/fr/lacombe/fraction/Fraction.java
@@ -8,14 +8,8 @@ public class Fraction {
     }
 
     public Fraction plus(Fraction fraction) {
-        if(this.value == 3){
-            return  new Fraction(3+fraction.getValue());
-        }
-        if(this.value == 3){
-            return  new Fraction(3+fraction.getValue());
-        }
-        if(this.value == 3){
-            return  new Fraction(3+fraction.getValue());
+        if (this.value == 3) {
+            return new Fraction(3 + fraction.getValue());
         }
 
         if(this.value == 0)
