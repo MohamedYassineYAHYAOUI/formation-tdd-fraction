48a6494a68ba1fe9da46b4c32e428d360a453c8a
Mohamed Yassine YAHYAOUI <yassine.yahyaoui@lacombedulionvert.fr>
2022-11-22 16:49:39 +0100

    refacto : test given_in_zero_zero_should_return_zero

--- src/test/java/fr/lacombe/fraction/AdditionTest.java
+++ src/test/java/fr/lacombe/fraction/AdditionTest.java
@@ -9,8 +9,7 @@ public class AdditionTest {
     @Test
     void given_in_zero_zero_should_return_zero(){
         Fraction fraction1 = new Fraction(0);
-        Fraction fraction2 = new Fraction(0);
-        Fraction sommeFraction = fraction1.addition(fraction2);
+        Fraction sommeFraction = fraction1.addition(fraction1);
 
         Assertions.assertEquals(0, sommeFraction.getValue());
     }
