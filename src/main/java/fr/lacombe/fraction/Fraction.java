a78f5d88043b67031b63d2fd04a195c5fd74c6fe
Mohamed Yassine YAHYAOUI <yassine.yahyaoui@lacombedulionvert.fr>
2022-11-22 16:10:57 +0100

    refacto: addition return value of input integer

--- src/main/java/fr/lacombe/fraction/Fraction.java
+++ src/main/java/fr/lacombe/fraction/Fraction.java
@@ -10,18 +10,7 @@ public class Fraction {
     }
 
     public Fraction addition(Fraction fraction) {
-        if(value == 5){
-            return new Fraction(value);
-        }
-
-        if(value == 3){
-            return new Fraction(value);
-        }
-
-        if(value == 4){
-            return new Fraction(value);
-        }
-        return new Fraction(0);
+        return new Fraction(value);
     }
 
     public int getValue() {
