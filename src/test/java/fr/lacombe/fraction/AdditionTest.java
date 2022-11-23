e1143ca3bda1ef6be84f7f9d7c2bf4cb24ef3702
Mohamed Yassine YAHYAOUI <yassine.yahyaoui@lacombedulionvert.fr>
2022-11-23 14:48:29 +0100

    refacto : given_two_fractions_with_same_denominator_should_return_a_fraction_with_same_denominator1

--- src/test/java/fr/lacombe/fraction/AdditionTest.java
+++ src/test/java/fr/lacombe/fraction/AdditionTest.java
@@ -19,7 +19,7 @@ public class AdditionTest {
     }
 
     @ParameterizedTest
-    @CsvSource({"1,2,6,7"})
+    @CsvSource({"1,2,6,7", "1,2,4,5","1,2,2,3"})
     void given_two_fractions_with_same_denominator_should_return_a_fraction_with_same_denominator(int numerator1, int denominator,
                                                                                                   int numerator2, int expectedNumerator){
         Fraction firstFraction = new Fraction(numerator1, denominator);
@@ -29,17 +29,6 @@ public class AdditionTest {
 
         Assertions.assertEquals(expectedNumerator, resultFraction.getNumerator());
         Assertions.assertEquals(denominator, resultFraction.getDenominator());
-    }
-
-    @Test
-    void given_two_fractions_with_same_denominator_should_return_a_fraction_with_same_denominator1(){
-        Fraction firstFraction = new Fraction(1,2);
-        Fraction secondFraction = new Fraction(6,2);
-
-        Fraction resultFraction = firstFraction.plus(secondFraction);
-
-        Assertions.assertEquals(7, resultFraction.getNumerator());
-        Assertions.assertEquals(2, resultFraction.getDenominator());
     }
 
 
