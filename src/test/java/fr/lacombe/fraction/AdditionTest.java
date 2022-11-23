4df472ebc40f57e3c78610abb5c76b40cef0a283
Mohamed Yassine YAHYAOUI <yassine.yahyaoui@lacombedulionvert.fr>
2022-11-23 11:42:57 +0100

    refacto: given_two_integers_should_return_sum_of_both

--- src/test/java/fr/lacombe/fraction/AdditionTest.java
+++ src/test/java/fr/lacombe/fraction/AdditionTest.java
@@ -18,16 +18,6 @@ public class AdditionTest {
         Assertions.assertEquals(expected,  resultFraction.getValue());
     }
 
-    @ParameterizedTest
-    @CsvSource({"3,3", "2,2", "1,1"})
-    void given_zero_and_integer_should_return_integer(int input, int expected){
-        Fraction zero = new Fraction(0);
-        Fraction givenFraction= new Fraction(input);
-
-        Fraction resultFraction = zero.plus(givenFraction);
-
-        Assertions.assertEquals(expected, resultFraction.getValue());
-    }
 
     @ParameterizedTest
     @CsvSource({"3,5,8", "3,1,4", "3,2,5", "0,0,0","0,3,3", "0,2,2", "0,1,1" })
