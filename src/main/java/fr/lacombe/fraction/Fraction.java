97ecf8e889885497be9a9056b45111c7645edebe
Mohamed Yassine YAHYAOUI <yassine.yahyaoui@lacombedulionvert.fr>
2022-11-23 16:02:23 +0100

    refacto : changer le nom de variable value

--- src/main/java/fr/lacombe/fraction/Fraction.java
+++ src/main/java/fr/lacombe/fraction/Fraction.java
@@ -1,35 +1,35 @@
 package fr.lacombe.fraction;
 
 public class Fraction {
-    private final int value;
+    private final int numerator;
     private int denominator;
 
-    public Fraction(int value) {
-        this.value = value;
+    public Fraction(int numerator) {
+        this.numerator = numerator;
 
     }
 
     public Fraction(int numerator, int denominator) {
-        this.value = numerator;
+        this.numerator = numerator;
         this.denominator = denominator;
     }
 
     public Fraction plus(Fraction fraction) {
         if(this.denominator == 2 && fraction.getDenominator() == 3) {
-           return new Fraction((this.value * fraction.getDenominator())+(fraction.getNumerator() * this.denominator),6);
+           return new Fraction((this.numerator * fraction.getDenominator())+(fraction.getNumerator() * this.denominator),6);
         }
         if(this.denominator == 4 && fraction.denominator == 8){
-            return new Fraction((this.value * fraction.getDenominator())+(fraction.getNumerator() *this.denominator),32);
+            return new Fraction((this.numerator * fraction.getDenominator())+(fraction.getNumerator() *this.denominator),32);
         }
-        return new Fraction(this.value + fraction.getValue(), denominator);
+        return new Fraction(this.numerator + fraction.getValue(), denominator);
     }
 
     public int getValue() {
-        return value;
+        return numerator;
     }
 
     public int getNumerator() {
-        return value;
+        return numerator;
     }
 
     public int getDenominator() {
