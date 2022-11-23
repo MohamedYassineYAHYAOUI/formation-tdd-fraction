4d494cc211c4378e6dc00203c35cf8cec578f5f6
Mohamed Yassine YAHYAOUI <yassine.yahyaoui@lacombedulionvert.fr>
2022-11-23 11:58:31 +0100

    feat: given negatif integer

--- src/test/java/fr/lacombe/fraction/AdditionTest.java
+++ src/test/java/fr/lacombe/fraction/AdditionTest.java
@@ -6,7 +6,8 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class AdditionTest {
     @ParameterizedTest
-    @CsvSource({"3,5,8", "3,1,4", "3,2,5", "0,0,0", "0,3,3", "5,0,5"})
+    @CsvSource({"3,5,8", "3,1,4", "3,2,5", "0,0,0", "0,3,3", "5,0,5",
+            "-2,1,-1", "-4,-1,-5",  "3,-1,2", "0,-2,-2"})
     void given_two_integers_should_return_sum_of_both(int input1, int input2, int expected) {
         Fraction firstFraction = new Fraction(input1);
         Fraction secondFraction = new Fraction(input2);
