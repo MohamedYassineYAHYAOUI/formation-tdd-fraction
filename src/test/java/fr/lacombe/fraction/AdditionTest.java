0c0ceeaaf9e6200856e97de6205b84fc26cf3003
Mohamed Yassine YAHYAOUI <yassine.yahyaoui@lacombedulionvert.fr>
2022-11-23 11:45:36 +0100

    refacto: format code

--- src/test/java/fr/lacombe/fraction/AdditionTest.java
+++ src/test/java/fr/lacombe/fraction/AdditionTest.java
@@ -1,34 +1,18 @@
 package fr.lacombe.fraction;
+
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class AdditionTest {
-
-
     @ParameterizedTest
-    @CsvSource({"5,5", "3,3", "4,4"})
-    void given_integer_plus_zero_should_return_integer(int input, int expected){
-        Fraction givenFraction = new Fraction(input);
-        Fraction zero = new Fraction(0);
-
-        Fraction resultFraction = givenFraction.plus(zero);
-
-        Assertions.assertEquals(expected,  resultFraction.getValue());
-    }
-
-
-    @ParameterizedTest
-    @CsvSource({"3,5,8", "3,1,4", "3,2,5", "0,0,0","0,3,3", "0,2,2", "0,1,1","5,0,5", "3,0,3", "4,0,4" })
-    void given_two_integers_should_return_sum_of_both(int input1, int input2, int expected){
+    @CsvSource({"3,5,8", "3,1,4", "3,2,5", "0,0,0", "0,3,3", "0,2,2", "0,1,1", "5,0,5", "3,0,3", "4,0,4"})
+    void given_two_integers_should_return_sum_of_both(int input1, int input2, int expected) {
         Fraction givenFirstFraction = new Fraction(input1);
-        Fraction givenSecondFraction= new Fraction(input2);
+        Fraction givenSecondFraction = new Fraction(input2);
 
         Fraction resultFraction = givenFirstFraction.plus(givenSecondFraction);
 
         Assertions.assertEquals(expected, resultFraction.getValue());
     }
-
-
 }
