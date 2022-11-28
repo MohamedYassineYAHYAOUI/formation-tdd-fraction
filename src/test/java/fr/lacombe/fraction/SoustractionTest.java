5ea7b1636862db464fd0dd324811f228ec93c10a
Mohamed Yassine YAHYAOUI <yassine.yahyaoui@lacombedulionvert.fr>
2022-11-28 14:38:08 +0100

    test  : refacto

--- src/test/java/fr/lacombe/fraction/SoustractionTest.java
+++ src/test/java/fr/lacombe/fraction/SoustractionTest.java
@@ -14,24 +14,4 @@ public class SoustractionTest {
 
         Assertions.assertEquals(-4, result.getNumerator());
     }
-
-    @Test
-    void integers2() {
-        Fraction firstFraction = Fraction.create(8);
-        Fraction secondFraction = Fraction.create(2);
-
-        Fraction result = firstFraction.minus(secondFraction);
-
-        Assertions.assertEquals(6, result.getNumerator());
-    }
-
-    @Test
-    void integers3() {
-        Fraction firstFraction = Fraction.create(-5);
-        Fraction secondFraction = Fraction.create(2);
-
-        Fraction result = firstFraction.minus(secondFraction);
-
-        Assertions.assertEquals(-7, result.getNumerator());
-    }
 }
