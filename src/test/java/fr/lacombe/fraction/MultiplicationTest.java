642961251bf84668d738a8364314350af6856a29
Mohamed Yassine YAHYAOUI <yassine.yahyaoui@lacombedulionvert.fr>
2022-11-28 16:57:46 +0100

    refacto : multiplication

--- src/test/java/fr/lacombe/fraction/MultiplicationTest.java
+++ src/test/java/fr/lacombe/fraction/MultiplicationTest.java
@@ -16,18 +16,20 @@ public class MultiplicationTest {
 
     @Test
     void negative() {
-
         Fraction firstFraction = Fraction.create(-1, 5);
         Fraction secondFraction = Fraction.create(1, 5);
         var expectedFraction = Fraction.create(-1, 25);
-        
+
         Assertions.assertEquals(expectedFraction, firstFraction.multiply(secondFraction));
     }
 
     @Test
     void twoNegative() {
         var expectedFraction = Fraction.create(3, 4);
-        Assertions.assertEquals(expectedFraction, Fraction.create(-5, 10).multiply(Fraction.create(3, -2)));
+        Fraction secondFraction = Fraction.create(3, -2);
+        Fraction firstFraction = Fraction.create(-5, 10);
+        
+        Assertions.assertEquals(expectedFraction, firstFraction.multiply(secondFraction));
     }
 
 }
