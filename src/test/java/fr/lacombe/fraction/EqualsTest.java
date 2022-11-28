1f6e1e303f371daad7a88f81aefe2f3b870d7f87
Mohamed Yassine YAHYAOUI <yassine.yahyaoui@lacombedulionvert.fr>
2022-11-28 16:15:29 +0100

    refacto: simplified Fraction

--- src/test/java/fr/lacombe/fraction/EqualsTest.java
+++ src/test/java/fr/lacombe/fraction/EqualsTest.java
@@ -3,7 +3,7 @@ package fr.lacombe.fraction;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class EqualtsTest {
+public class EqualsTest {
 
     @Test
     void sameFraction() {
@@ -14,4 +14,11 @@ public class EqualtsTest {
     void simplifiedFraction() {
         Assertions.assertEquals(Fraction.create(1, 3), Fraction.create(2, 6));
     }
+
+    @Test
+    void differentFraction() {
+        Assertions.assertNotEquals(Fraction.create(2, 5), Fraction.create(5, 2));
+    }
+
+
 }
