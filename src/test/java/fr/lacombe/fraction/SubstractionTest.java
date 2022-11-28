64b2010d768e6e38cabd0bc0aef4ed0cfe6692c6
Mohamed Yassine YAHYAOUI <yassine.yahyaoui@lacombedulionvert.fr>
2022-11-28 15:19:34 +0100

    refacto test substraction

--- src/test/java/fr/lacombe/fraction/SubstractionTest.java
+++ src/test/java/fr/lacombe/fraction/SubstractionTest.java
@@ -14,31 +14,9 @@ public class SubstractionTest {
 
         Assertions.assertEquals(-4, result.getNumerator());
     }
-
+    
     @Test
     void sameDenominator() {
-        Fraction firstFraction = Fraction.create(1, 2);
-        Fraction secondFraction = Fraction.create(5, 2);
-
-        Fraction result = firstFraction.minus(secondFraction);
-
-        Assertions.assertEquals(-2, result.getNumerator());
-        Assertions.assertEquals(1, result.getDenominator());
-    }
-
-    @Test
-    void sameDenominator2() {
-        Fraction firstFraction = Fraction.create(-3, 4);
-        Fraction secondFraction = Fraction.create(-1, 4);
-
-        Fraction result = firstFraction.minus(secondFraction);
-
-        Assertions.assertEquals(-1, result.getNumerator());
-        Assertions.assertEquals(2, result.getDenominator());
-    }
-
-    @Test
-    void sameDenominator3() {
         Fraction firstFraction = Fraction.create(-1, 3);
         Fraction secondFraction = Fraction.create(1, 3);
 
