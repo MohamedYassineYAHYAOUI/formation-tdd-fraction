014bfc5c98ae75bb50b921bf15e82cb736feb582
Mohamed Yassine YAHYAOUI <yassine.yahyaoui@lacombedulionvert.fr>
2022-11-28 17:49:39 +0100

    feat sameDenominator test

--- src/test/java/fr/lacombe/fraction/DivisionTest.java
+++ src/test/java/fr/lacombe/fraction/DivisionTest.java
@@ -1,11 +1,17 @@
 package fr.lacombe.fraction;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DivisionTest {
     @Test
     void integers() {
-        assertEquals(Fraction.create(5,2),Fraction.create(5).divide(Fraction.create(2)));
+        assertEquals(Fraction.create(5, 2), Fraction.create(5).divide(Fraction.create(2)));
+    }
+
+    @Test
+    void sameDenominator() {
+        assertEquals(Fraction.create(1), Fraction.create(1, 4).divide(Fraction.create(1, 4)));
     }
 }
